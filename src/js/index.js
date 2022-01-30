513cff9d5f840e8bb3784cd4436852b299575157
angelique-df <faye.angelique.d@gmail.com>
2022-01-30 22:31:11 +0000

    implemented dark theme

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,9 @@
+// TODO implement adding tasks
+
 // TODO change ".task-state label" appearance when ".task-state label > input" is selected
+
+// TODO change .tasks-count according to number of tasks unchecked
+
+// TODO remove checked on .clear-tasks click
+
+// TODO implement tasks filtering
