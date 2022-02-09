36a68f2d979cf696b5af1b297b57664142dc4670
angelique-df <faye.angelique.d@gmail.com>
2022-02-09 12:43:09 +0000

    Created components for each part of the app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,87 @@
 import logo from './logo.svg';
 import './App.css';
 
+const Header = () => {
+  return (
+    <header>
+      <div>
+        <h1>Todo</h1>
+        <button type="button" class="toggle-dark-theme" aria-label="Change theme">
+        </button>
+      </div>
+    </header>
+  )
+};
+
+const AddTask = () => {
+
+  return (
+    <form action="" class="new-task">
+      <div class="checkbox"></div>
+      <input type="text" placeholder="Create a new todo..." />
+    </form>
+  )
+};
+
+const DeleteTasks = () => {
+
+  return (
+    <form id="delete-checked-tasks-form" action="" class="delete-checked-tasks-form">
+      <ul>
+
+        <li class="single-task">
+
+          {/* drag and drop events are attached to inputs */}
+          <input type="checkbox" class="task-checkbox" name="" />
+
+          {/* clicking on label checks the adjacent input */}
+          <label for="">
+            Complete online JavaScript course
+          </label>
+
+          <button type="button" class="delete-task"></button>
+        </li>
+      </ul>
+    </form>
+  )
+};
+
+const TasksInfo = () => {
+  return (
+    <div class="tasks-state">
+
+      <div class="tasks-info">
+        <p class="tasks-count">
+          {/* <!-- Add dynamic number -->  */} 2 items left
+        </p>
+
+        <button form="delete-checked-tasks-form" type="submit" class="clear-tasks">
+          Clear Completed
+        </button>
+      </div>
+
+      <div class="tasks-filter">
+        <button type="button" class="filter-applied">All</button>
+        <button type="button">Active</button>
+        <button type="button">Completed</button>
+      </div>
+
+    </div>
+  )
+};
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <body className="App">
+        <Header />
+        <main>
+          <AddTask />
+          <DeleteTasks />
+          <TasksInfo />
+        </main>
+      </body>
+    </>
   );
 }
 
