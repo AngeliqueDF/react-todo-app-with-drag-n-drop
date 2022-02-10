fe76ce4b28beb51b2be5d65437e07846d4b94a08
angelique-df <faye.angelique.d@gmail.com>
2022-02-10 15:03:47 +0000

    Update AddTask.js

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -2,10 +2,10 @@ import React from 'react'
 
 const AddTask = ({ addTask }) => {
   return (
-    <form autocomplete="off" className="new-task" onSubmit={addTask}>
+    <form autoComplete="off" className="new-task" onSubmit={addTask} >
       <div className="checkbox"></div>
       <input type="text" name='newTodo' placeholder="Create a new todo..." required />
-    </form>
+    </form >
   )
 };
 
